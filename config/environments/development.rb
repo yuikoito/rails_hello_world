bd0a8edfc1f31d4231bd8aba96e75f7c44b02e82
yuikoito <toffywing@gmail.com>
2021-12-13 16:27:36 +0900

    chore: hello world

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,6 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+  # ローカルWebサーバーへの接続を許可する
+  config.hosts.clear
 end
